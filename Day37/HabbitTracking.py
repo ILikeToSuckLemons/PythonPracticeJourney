88f5a18e294da9847823b02a1fb6d0b25a174be5
James Latanna <jamesgetgit@gmail.com>
2025-03-25 23:15:53 +0900

    first commit

---
+++ Day37/HabbitTracking.py
@@ -0,0 +1,25 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+username = "james13"
+TOKEN = "jamesgetgit"
+userparams = {
+    "token": "jamesgetgit",
+    "username": "james13",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+# response = requests.post(url=pixela_endpoint, json=userparams)
+# print(response.text)
+
+graphendpoint = f"{pixela_endpoint}/{username}/graphs"
+
+
+graphconfig = {
+    "id": "graph1",
+    "name": "Graf",
+    "unit": "day",
+    "type": "float",
+    "color": "sora"
+}
+requests.post()
